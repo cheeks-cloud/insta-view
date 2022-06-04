1cfd046588414cde0058b5086061f7a5048eba56
@cheeks cloud <agneskaswii700@gmail.com>
2022-06-04 10:11:17 +0300

    adding pillow

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# class Profile(models.Model):
+#   photo = models.ImageField(null=False, blank=False, upload_to='images')
+#   bio = models.TextField(null=False, blank=False)
+
+
+# class Image(models.Model):
+#   image = models.ImageField(null=False, blank=False, upload_to='images/')
+#   name = models.CharField(max_length=40,null=False, blank=False)
+#   caption = models.TextField(null=False, blank=False)
+#   profile = models.ForeignKey(Profile, on_delete = models.CASCADE)
+#   comments = models.TextField(null=False, blank=False)
+#   likes = models.IntegerField(null=False, blank=False)
+
+
+
+
