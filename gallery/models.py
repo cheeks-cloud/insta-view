970b1705bc0f427c72de76be4ba07b59c5ddf40d
@cheeks cloud <agneskaswii700@gmail.com>
2022-06-04 10:47:57 +0300

    creating admin

--- gallery/models.py
+++ gallery/models.py
@@ -5,6 +5,9 @@ class Profile(models.Model):
   bio = models.TextField(null=False, blank=False)
 
 
+  def __str__(self):
+      return self.photo
+
 class Image(models.Model):
   image = models.ImageField(null=False, blank=False, upload_to='images/')
   name = models.CharField(max_length=40,null=False, blank=False)
@@ -12,6 +15,20 @@ class Image(models.Model):
   profile = models.ForeignKey(Profile, on_delete = models.CASCADE)
   comments = models.TextField(null=False, blank=False)
   likes = models.IntegerField(null=False, blank=False)
+
+
+  def save_image(self):
+    self.save()
+
+  def delete_image(self):
+     Image.objects.get(id = self.id).delete()
+
+  def update_captions(self):
+
+
+
+   def __str__(self):
+      return self.image
 
 
 
