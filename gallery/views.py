bf95bb66f1de901ff62c1dee498d5dabf8ee1c54
@cheeks cloud <agneskaswii700@gmail.com>
2022-06-04 13:01:22 +0300

    creating login page

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Image,Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def welcome(request):
