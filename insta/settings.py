c798e23b1a4c2710390dd7bf09fd0b07e49b14ca
@cheeks cloud <agneskaswii700@gmail.com>
2022-06-04 09:51:17 +0300

    connecting to the database

--- insta/settings.py
+++ insta/settings.py
@@ -58,8 +58,10 @@ WSGI_APPLICATION = 'insta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'galleria',
+        'USER': 'moringa',
+        'PASSWORD':'cheeks',
     }
 }
 
